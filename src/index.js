def8c8e5ccacd57ac044a9bc0ce301cb065e6bf8
gion <bogdan.gradinariu@gmail.com>
2018-06-06 16:45:10 +0300

    1.0.2

--- src/index.js
+++ src/index.js
@@ -1,27 +1,2 @@
-// adapted from https://stackoverflow.com/a/3943023/491075
-export default isDarkColor = hexColor => {
-  const { r, g, b } = hexToRgb(hexColor)
-
-  let colorArray = [r / 255, g / 255, b / 255].map(v => {
-    if (v <= 0.03928) {
-      return v / 12.92
-    }
-
-    return Math.pow((v + 0.055) / 1.055, 2.4)
-  })
-
-  const luminance = 0.2126 * colorArray[0] + 0.7152 * colorArray[1] + 0.0722 * colorArray[2]
-
-  return luminance <= 0.179
-}
-
-// credits go to https://stackoverflow.com/a/5624139/491075
-const hexToRgb = hex => {
-  const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex)
-
-  return result ? {
-    r: parseInt(result[1], 16),
-    g: parseInt(result[2], 16),
-    b: parseInt(result[3], 16),
-  } : null
-}
+import { isDarkColor } from './isDarkColor'
+export default isDarkColor
