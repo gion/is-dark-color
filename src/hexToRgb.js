aa2ce121406dba7b1f07c53cbc1875fecd4ffbdb
Ronan McCarter <rpmccarter@gmail.com>
2023-08-21 09:54:15 -0700

    add handling for shorthand hex

--- src/hexToRgb.js
+++ src/hexToRgb.js
@@ -1,10 +1,15 @@
 // credits go to https://stackoverflow.com/a/5624139/491075
 export const hexToRgb = hex => {
-  const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex)
+  // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
+  var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+  hex = hex.replace(shorthandRegex, (m, r, g, b) => {
+    return r + r + g + g + b + b;
+  });
 
+  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
   return result ? {
     r: parseInt(result[1], 16),
     g: parseInt(result[2], 16),
     b: parseInt(result[3], 16),
-  } : null
+  } : null;
 }
